7c75c67c0adb71ff81c565cc1e5259026e65d3e7
Klushin Valdimir <vova_klushin@mail.ru>
2023-03-03 14:57:32 +0300

    auth in VK

--- app/src/main/java/com/example/testvk/MainActivity.kt
+++ app/src/main/java/com/example/testvk/MainActivity.kt
@@ -1,4 +1,11 @@
 package com.example.testvk
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+
 class MainActivity : AppCompatActivity(R.layout.activity_main)
--- app/src/main/java/com/example/testvk/MainFragment.kt
+++ app/src/main/java/com/example/testvk/MainFragment.kt
@@ -1,23 +1,29 @@
 package com.example.testvk
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.testvk.databinding.MainFragmentBinding
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 import java.io.IOException
 
-class MainFragment(): Fragment(R.layout.main_fragment) {
+class MainFragment() : Fragment(R.layout.main_fragment) {
 
+    private lateinit var authLauncher: ActivityResultLauncher<Collection<VKScope>>
 
-    private val binding : MainFragmentBinding by viewBinding(MainFragmentBinding::bind)
+    private val binding: MainFragmentBinding by viewBinding(MainFragmentBinding::bind)
     private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
@@ -25,6 +31,19 @@ class MainFragment(): Fragment(R.layout.main_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        authLauncher = VK.login(requireActivity()) { result ->
+            when (result) {
+                is VKAuthenticationResult.Success -> {
+                    Toast.makeText(requireContext(), "success auth", Toast.LENGTH_SHORT).show()
+                }
+                is VKAuthenticationResult.Failed -> {
+                    showLoginDialog()
+                }
+            }
+        }
+
+        showLoginDialog()
 
         mediaRecorder = MediaRecorder()
         output = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
@@ -35,17 +54,26 @@ class MainFragment(): Fragment(R.layout.main_fragment) {
         mediaRecorder?.setOutputFile(output)
 
         binding.buttonStartRecording.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(requireContext(),
-                    android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(requireContext(),
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                ActivityCompat.requestPermissions(requireActivity(), permissions,0)
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val permissions = arrayOf(
+                    android.Manifest.permission.RECORD_AUDIO,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                ActivityCompat.requestPermissions(requireActivity(), permissions, 0)
             } else {
                 startRecording()
             }
         }
 
-        binding.buttonStopRecording.setOnClickListener{
+        binding.buttonStopRecording.setOnClickListener {
             stopRecording()
         }
 
@@ -71,7 +99,7 @@ class MainFragment(): Fragment(R.layout.main_fragment) {
     private fun pauseRecording() {
         if (state) {
             if (!recordingStopped) {
-                Toast.makeText(requireContext(),"Stopped!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Stopped!", Toast.LENGTH_SHORT).show()
                 mediaRecorder?.pause()
                 recordingStopped = true
                 binding.buttonPauseRecording.text = "Resume"
@@ -83,19 +111,33 @@ class MainFragment(): Fragment(R.layout.main_fragment) {
 
     @SuppressLint("RestrictedApi", "SetTextI18n")
     private fun resumeRecording() {
-        Toast.makeText(requireContext(),"Resume!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(), "Resume!", Toast.LENGTH_SHORT).show()
         mediaRecorder?.resume()
         binding.buttonPauseRecording.text = "Pause"
         recordingStopped = false
     }
 
-    private fun stopRecording(){
+    private fun stopRecording() {
         if (state) {
             mediaRecorder?.stop()
             mediaRecorder?.release()
             state = false
         } else {
-            Toast.makeText(requireContext(), "You are not recording right now!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "You are not recording right now!", Toast.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    private fun showLoginDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Login")
+            .setMessage("To use this app log in VK")
+            .setPositiveButton("Lets go") { _, _ ->
+                authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+            }
+            .setNegativeButton("Nah") { _, _ ->
+                requireActivity().finish()
+            }
+            .show()
     }
 }
